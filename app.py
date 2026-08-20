bc1100d6e04a80e43586afcd71aebd52b45f5950
larissamariana-sudo <larissamariana@gmail.com>
2026-08-19 22:01:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ df_site = carregar_dados_planilha()
 # ==========================================
 # 1. FOTO DE CAPA DO SITE
 # ==========================================
-# Para substituir: st.image("imagens/arlivre.png", use_container_width=True)
+# Para substituir: st.image("arlivre.png", use_container_width=True)
 st.markdown('<div class="placeholder-img">🖼️ Coloque sua foto de capa aqui (Sugestão: 1200x300px)</div>', unsafe_allow_html=True)
 
 st.markdown('<h1 class="main-header">Prof. Dr(a). Larissa Mariana Veloso de Oliveira</h1>', unsafe_allow_html=True)
